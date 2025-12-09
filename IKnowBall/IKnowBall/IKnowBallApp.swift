8a1b33ee779387fe7ed9d0be52adc1bee08445e4
Brentthomas248 <brentthomas248@gmail.com>
2025-12-08 22:02:05 -0600

    Xcode

---
+++ IKnowBall/IKnowBall/IKnowBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  IKnowBallApp.swift
+//  IKnowBall
+//
+//  Created by Brent Showalter on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IKnowBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
